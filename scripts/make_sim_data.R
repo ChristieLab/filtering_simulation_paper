981b5f5decb3f191456215702b41f6b95e6b87f1
William Beryl Hemstrom <whemstro@bell-fe03.rcac.purdue.edu>
2023-03-29 21:14:35 -0400

    updated scripts and added resulting raw sim data

--- scripts/make_sim_data.R
+++ scripts/make_sim_data.R
@@ -1,14 +1,15 @@
 library(coala); library(foreach); library(doParallel); library(parallel)
 # comp <- data.table::fread("../aus_monarchs/data/genotypes.geno")
 
-lens <- read.table("data/mon_chr_lengths.txt")
+lens <- read.table("/scratch/bell/whemstro/filtering_simulation_paper/data/mon_chr_lengths.txt")
 
 
 coala::activate_scrm()
 
 mu <- 2.9e-9
 N0 <- 1e4
-m <- .05
+m <- 10/N0
+bottle <- 0.01
 
 ntasks <- 30
 
@@ -16,18 +17,20 @@ cl <- parallel::makePSOCKcluster(ntasks)
 doParallel::registerDoParallel(cl)
 
 #loop through each set
-out <- foreach::foreach(q = 1:ntasks) %dopar% {
+out <- foreach::foreach(q = 1:ntasks, .packages = "coala") %dopar% {
   tlen <- lens$V2[q]
   d <- coal_model(c(100, 100), loci_number = 1, loci_length = tlen, ploidy = 2) +
     sumstat_seg_sites() +
     feat_mutation(4*N0*mu*tlen) + 
-    feat_migration(4*N0*m, symmetric = TRUE) + 
-    feat_size_change(.1, population = 2, time = .3) +
-    feat_size_change(1, population = 2, time = .5) +
+    feat_migration(4*N0*m, symmetric = TRUE) +
+    feat_pop_merge(.8, pop_source = 2, 1) +
+    feat_size_change(1, population = 2, time = .02) +
+    feat_size_change(bottle, population = 2, time = .01) +
+    feat_size_change(1, population = 2, time = 0) +
     feat_recombination(4*N0, locus_group = 1)
   
   res <- simulate(d)
   res$seg_sites
 }
 
-saveRDS(out, "mig_sim_dat.RDS")
+saveRDS(out, "/scratch/bell/whemstro/filtering_simulation_paper/data/raw_mig_sim_dat.RDS")
