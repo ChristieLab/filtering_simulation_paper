7301459349f5d139e3d804747efacc1e4edae787
hemstrow <hemstrow@gmail.com>
2023-06-07 16:00:46 -0700

    added the LCT data and some scripts

--- scripts/process_maf_res.R
+++ scripts/process_maf_res.R
@@ -1,132 +1,225 @@
-library(data.table); library(ggplot2)
+library(data.table); library(ggplot2); library(mgcv); library(tidygam); library(gridExtra)
 
 fl <- list.files("results/", "\\.RDS", full.names = TRUE)
 r <- vector("list", length(fl))
+
+bind_parms <- function(y, parms, i){
+  y$maf <- parms[1,4]
+  y$mac <- ifelse(parms[1,5] == 0, FALSE, parms[1,5])
+  y$hwe <- parms[1,6]
+  y$miss <- parms[1,8]
+  y$i <- i
+  return(y)
+}
 
 for(i in 1:length(fl)){
   print(i)
   
   y <- readRDS(fl[i])
-  y$ld <- NULL # skip for now, too big
-  if(!is.null(y$PCA)){
-    y$pca <- y$pca$data[,c("samp", "pop", "PC1", "PC2")]
-    y$pca$mac <- y$stats$mac[1]
-    y$pca$maf <- y$stats$maf[1]
+  
+  uf <- unique(y$res$ld$sample.subfacet)
+  nd <- vector("list", length(uf))
+  for(j in 1:length(uf)){
+    if(nrow(y$res$ld[y$res$ld$sample.subfacet == uf[j],]) > 2){
+      ldm <- gam(formula = CLD ~ s(proximity, bs = "cs"), data = y$res$ld[y$res$ld$sample.subfacet == uf[j],])
+      nd[[j]] <- tidygam::predict_gam(ldm, 100)
+      nd[[j]]$sample.subfacet <- uf[j]
+    }
   }
-  if(ncol(y$sfs) == 5){
-    colnames(y$sfs) <- c("pop", "n_min_al", "count", "maf", "mac")
+  nd <- rbindlist(nd)
+  
+  r[[i]]$ld <- bind_parms(nd, y$parms, i)
+  r[[i]]$stats <- bind_parms(y$res$stats, y$parms, i)
+  y$res$sfs$mac.1 <- NULL
+  colnames(y$res$sfs)[which(colnames(y$res$sfs) == "mac")] <- "min_allele_count"
+  r[[i]]$sfs <- bind_parms(y$res$sfs, y$parms, i)
+  if(!is.null(y$res$pca)){
+    r[[i]]$pca <- bind_parms(as.data.frame(y$res$pca$data[,1:5]), y$parms, i)
   }
-  else{
-    colnames(y$sfs) <- c("n_min_al", "count", "maf", "mac")
-    
-  }
-  r[[i]] <- y
+  
   names(r)[i] <- fl[i]
 }
 
-lseq <- function(from, to, length.out ) exp(seq(log(from), log(to), length.out = length.out))
-mafs <- seq(0, .1, by = .01)
-macs <- c(1, 3, 5)
-hwes <- lseq(1e-6, .05, 10)
+saveRDS(r, "data/processed/filt_results.RDS")
 
 stats <- purrr::map(r, "stats")
 pca <- purrr::map(r, "pca")
 sfs <- purrr::map(r, "sfs")
+ld <- purrr::map(r, "ld")
 
-iter <- gsub(".+res_r", "", fl)
-iter <- gsub("\\.RDS", "", iter)
-iter <- as.numeric(iter)
+stats <- data.table::rbindlist(stats)
+pca <- data.table::rbindlist(pca, fill = TRUE)
+sfs <- data.table::rbindlist(sfs, fill = TRUE)
+ld <- data.table::rbindlist(ld)
 
-names(stats) <- iter
-names(pca) <- iter
-names(sfs) <- iter
-stats <- data.table::rbindlist(stats, idcol = "iter")
-pca <- data.table::rbindlist(pca, idcol = "iter")
-sfs <- data.table::rbindlist(sfs, idcol = "iter", fill = TRUE)
-
-stats$subfacet[stats$subfacet == ".base"] <- "NUB"
-sfs$pop[is.na(sfs$pop)] <- "NUB"
-
-hwe_tab <- data.frame(iter = 1:sum(length(mafs), length(macs), length(hwes)))
-hwe_tab$hwe[hwe_tab$iter <= length(macs) + length(mafs)] <- 1e-6
-hwe_tab$hwe[(length(macs) + length(mafs) + 1):sum(length(mafs), length(macs), length(hwes))] <- hwes
-
-stats$iter <- as.numeric(stats$iter)
-stats$hwe <- hwe_tab$hwe[match(stats$iter, hwe_tab$iter)]
-sfs$hwe <- as.numeric(sfs$iter)
-sfs$hwe <- hwe_tab$hwe[match(sfs$iter, hwe_tab$iter)]
-
+# stats$subfacet[stats$subfacet == ".base"] <- "NUB"
+# sfs$pop[is.na(sfs$pop)] <- "NUB"
 
 stats$variable <- gsub("weighted_mean_", "", stats$variable)
-plot_stats <- c("D", "ho", "num_seg")
+plot_stats <- c("Tajima*'\\''*'s'*' D'", "H[O]", "N[seg]")
 
-ggplot(stats[stats$mac == 0 & !grepl("~", stats$subfacet) & stats$variable %in% plot_stats &
-               stats$hwe == 1e-6,], 
-       aes(x = maf, y = value, color = subfacet)) +
+stats$dataset <- ifelse(stats$subfacet %in% c("NUB", "GBT", "GBT~NUB"), "Yellow Perch", "Monarchs")
+var_tab <- data.frame(ref = unique(stats$variable),
+                      var_p = c("pi", "H[O]", "F[IS]", "F[ST]", "Tajima*'\\''*'s'*' D'", "N[seg]", "Tajima*'\\''*'s'*' 'theta",
+                                "Watterson*'\\''*'s'*' 'theta", "N[e]", "N[snp]"))
+
+stats$variable <- var_tab$var_p[match(stats$variable, var_tab$ref)]
+
+# maf
+pa <- ggplot(stats[stats$mac == FALSE & !grepl("~", stats$subfacet) & stats$variable %in% plot_stats &
+                     stats$hwe == 1e-6 & stats$miss == .5,], 
+             aes(x = maf, y = value, color = dataset, group = subfacet)) +
   geom_line(size = 1) +
-  facet_wrap(~variable, scales = "free_y", strip.position = "left") +
+  geom_point(size = 3) +
+  facet_wrap(~variable, scales = "free_y", strip.position = "left", labeller = label_parsed) +
   theme_bw() +
-  scale_color_brewer(palette = "Dark2", name = "Population") +
+  khroma::scale_color_highcontrast() +
   theme(axis.title.y = element_blank(),
         strip.background = element_blank(), 
         strip.placement = "outside",
         strip.text = element_text(size = 18),
         axis.title.x = element_text(size = 18),
-        axis.text = element_text(size = 14)) +
+        axis.text = element_text(size = 14),
+        legend.text = element_text(size = 14),
+        legend.title = element_text(size = 18)) +
   xlab("MAF filter")
 
-ggplot(stats[!grepl("~", stats$subfacet) & stats$variable %in% plot_stats &
-               stats$iter >= 15,], 
+# miss
+pb <- ggplot(stats[stats$mac == 1 & !grepl("~", stats$subfacet) & stats$variable %in% plot_stats &
+               stats$hwe == 1e-6,], 
+       aes(x = 1-miss, y = value, color = dataset, group = subfacet)) +
+  geom_line(size = 1) +
+  geom_point(size = 3) +
+  facet_wrap(~variable, scales = "free_y", strip.position = "left", labeller = label_parsed) +
+  scale_x_reverse() +
+  theme_bw() +
+  khroma::scale_color_highcontrast() +
+  theme(axis.title.y = element_blank(),
+        strip.background = element_blank(), 
+        strip.placement = "outside",
+        strip.text = element_text(size = 18),
+        axis.title.x = element_text(size = 18),
+        axis.text = element_text(size = 14),
+        legend.text = element_text(size = 14),
+        legend.title = element_text(size = 18)) +
+  xlab("Maximum proportion missing data")
+
+# hwe
+ggplot(stats[!grepl("~", stats$subfacet) & stats$variable %in% plot_stats & 
+               stats$mac == 1 & stats$maf == FALSE & stats$miss == .5,],
        aes(x = log10(hwe), y = value, color = subfacet)) +
   geom_line(size = 1) +
   facet_wrap(~variable, scales = "free_y", strip.position = "left") +
   theme_bw() +
-  scale_color_brewer(palette = "Dark2", name = "Population") +
+  scale_color_brewer(palette = "Set3", name = "Population") +
   theme(axis.title.y = element_blank(),
-        strip.background = element_blank(), 
+        strip.background = element_blank(),
         strip.placement = "outside",
         strip.text = element_text(size = 18),
         axis.title.x = element_text(size = 18),
         axis.text = element_text(size = 14)) +
   xlab("HWP p-value alpha")
 
-ggplot(stats[stats$mac == 0 & stats$variable == "fst"&
-               stats$hwe == 1e-6,], 
-       aes(x = maf, y = value, color = subfacet)) +
+# fst maf
+pa2 <- ggplot(stats[stats$mac == FALSE & grepl("~", stats$subfacet) & stats$variable == "F[ST]" &
+               stats$hwe == 1e-6 & stats$miss == .5,], 
+       aes(x = maf, y = value, color = dataset, group = subfacet)) +
   geom_line(size = 1) +
-  theme(
-        axis.title = element_text(size = 18),
-        axis.text = element_text(size = 14)) +
+  geom_point(size = 3) +
+  facet_wrap(~variable, scales = "free_y", strip.position = "left", labeller = label_parsed) +
   theme_bw() +
-  scale_color_brewer(palette = "Dark2", name = "Population") +
+  theme(axis.title.y = element_blank(),
+        strip.background = element_blank(), 
+        strip.placement = "outside",
+        strip.text = element_text(size = 18),
+        axis.title.x = element_text(size = 18),
+        axis.text = element_text(size = 14),
+        legend.text = element_text(size = 14),
+        legend.title = element_text(size = 18)) +
+  khroma::scale_color_highcontrast() +
   xlab("MAF filter") +
   ylab("Fst")
 
-ggplot(stats[stats$variable == "fst"&
-               stats$iter >= 15,], 
+
+# fst miss
+pb2 <- ggplot(stats[stats$mac == 1 & grepl("~", stats$subfacet) & stats$variable == "F[ST]" &
+               stats$hwe == 1e-6,], 
+       aes(x = 1-miss, y = value, color = dataset, group = subfacet)) +
+  geom_line(size = 1) +
+  geom_point(size = 3) +
+  facet_wrap(~variable, scales = "free_y", strip.position = "left", labeller = label_parsed) +
+  scale_x_reverse() +
+  theme_bw() +
+  khroma::scale_color_highcontrast() +
+  theme(axis.title.y = element_blank(),
+        strip.background = element_blank(), 
+        strip.placement = "outside",
+        strip.text = element_text(size = 18),
+        axis.title.x = element_text(size = 18),
+        axis.text = element_text(size = 14),
+        legend.text = element_text(size = 14),
+        legend.title = element_text(size = 18)) +
+  xlab("Maximum proportion missing data")
+
+# fst hwe
+ggplot(stats[grepl("~", stats$subfacet) &
+               stats$mac == 1 & stats$maf == FALSE & stats$miss == .5,],
        aes(x = log10(hwe), y = value, color = subfacet)) +
   geom_line(size = 1) +
-  theme(
-    axis.title = element_text(size = 18),
-    axis.text = element_text(size = 14)) +
+  facet_wrap(~variable, scales = "free_y", strip.position = "left") +
   theme_bw() +
-  scale_color_brewer(palette = "Dark2", name = "Population") +
-  xlab("HWP p-value alpha") +
-  ylab("Fst")
-  
-ggplot(pca[pca$mac == 0,], 
+  scale_color_brewer(palette = "Set3", name = "Population") +
+  theme(axis.title.y = element_blank(),
+        strip.background = element_blank(),
+        strip.placement = "outside",
+        strip.text = element_text(size = 18),
+        axis.title.x = element_text(size = 18),
+        axis.text = element_text(size = 14)) +
+  xlab("HWP p-value alpha")
+
+
+# pca maf
+ggplot(pca[pca$mac == FALSE & 
+             pca$hwe == 1e-6 & pca$miss == .5,], 
        aes(x = PC1, y = PC2, color = pop)) +
   geom_point() +
   facet_wrap(~maf) +
   theme_bw() +
-  scale_color_brewer(palette = "Dark2", name = "Population") +
+  scale_color_brewer(palette = "Set3", name = "Population") +
   theme(strip.background = element_blank(),
         strip.text = element_text(size = 18),
         axis.title = element_text(size = 18),
         axis.text = element_text(size = 14))
 
-ggplot(sfs[sfs$mac == 0 & sfs$hwe == 1e-6,], 
-       aes(x = n_min_al, y = log10(count), color = maf, group = maf)) +
+# pca hwe
+ggplot(pca[pca$mac == 1 & pca$miss == .5,], 
+       aes(x = PC1, y = PC2, color = pop)) +
+  geom_point() +
+  facet_wrap(~hwe) +
+  theme_bw() +
+  scale_color_brewer(palette = "Set3", name = "Population") +
+  theme(strip.background = element_blank(),
+        strip.text = element_text(size = 18),
+        axis.title = element_text(size = 18),
+        axis.text = element_text(size = 14))
+
+
+# pca hwe
+ggplot(pca[pca$mac == 1 & pca$hwe == 1e-6,], 
+       aes(x = PC1, y = PC2, color = pop)) +
+  geom_point() +
+  facet_wrap(~miss) +
+  theme_bw() +
+  scale_color_brewer(palette = "Set3", name = "Population") +
+  theme(strip.background = element_blank(),
+        strip.text = element_text(size = 18),
+        axis.title = element_text(size = 18),
+        axis.text = element_text(size = 14))
+
+
+# sfs maf
+ggplot(sfs[sfs$mac == FALSE & sfs$hwe == 1e-6 & sfs$miss == .5,], 
+       aes(x = min_allele_count, y = log10(count), color = maf, group = maf)) +
   geom_line(size = 1) +
   theme_bw() +
   facet_wrap(~pop, scale="free") +
@@ -137,14 +230,73 @@ ggplot(sfs[sfs$mac == 0 & sfs$hwe == 1e-6,],
   ylab("log10(Number of SNPs)")
 
 
-ggplot(sfs[sfs$iter >= 15,], 
-       aes(x = n_min_al, y = log10(count), color = log10(hwe), group = iter)) +
+# sfs hwe
+ggplot(sfs[sfs$mac == 1 & sfs$miss == .5 & sfs$maf == FALSE,], 
+       aes(x = min_allele_count, y = log10(count), color = log10(hwe), group = hwe)) +
   geom_line(size = 1) +
   theme_bw() +
   facet_wrap(~pop, scale="free") +
-  khroma::scale_color_batlow(name = "HWE filter") +
+  khroma::scale_color_batlow(name = "MAF filter") +
   theme(axis.title = element_text(size = 18),
         axis.text = element_text(size = 14)) +
   xlab("Minor Allele Count") +
   ylab("log10(Number of SNPs)")
 
+
+# sfs miss
+ggplot(sfs[sfs$mac == 1 & sfs$hwe == 1e-6 & sfs$maf == FALSE,], 
+       aes(x = min_allele_count, y = log10(count), color = miss, group = miss)) +
+  geom_line(size = 1) +
+  theme_bw() +
+  facet_wrap(~pop, scale="free") +
+  khroma::scale_color_batlow(name = "MAF filter") +
+  theme(axis.title = element_text(size = 18),
+        axis.text = element_text(size = 14)) +
+  xlab("Minor Allele Count") +
+  ylab("log10(Number of SNPs)")
+
+
+
+
+pl1 <- ggpubr::get_legend(pa)
+pl2 <- ggpubr::get_legend(pa2)
+
+
+grid.arrange(pa + guides(color = "none"), 
+             pb + guides(color = "none"), 
+             pa2 + guides(color = "none"), 
+             pb2 + guides(color = "none"),
+             pl1,
+             layout_matrix = matrix(c(1,2,3,4,5,5), nrow = 2),
+             widths = c(1,  .33333, .2))
+
+
+ggplot(ld[ld$mac == FALSE &
+                  ld$hwe == 1e-6 & ld$miss == .5,], 
+             aes(x = proximity, y = CLD, color = maf, ymin = lower_ci, ymax = upper_ci, group = maf)) +
+  geom_line(size = 1) +
+  facet_wrap(~sample.subfacet, strip.position = "left", scales = "free") +
+  theme_bw() +
+  khroma::scale_color_batlow(name = "MAF filter") +
+  theme(axis.title.y = element_blank(),
+        strip.background = element_blank(), 
+        strip.placement = "outside",
+        strip.text = element_text(size = 18),
+        axis.title.x = element_text(size = 18),
+        axis.text = element_text(size = 14),
+        legend.text = element_text(size = 14),
+        legend.title = element_text(size = 18)) +
+  xlab("Proximity (bp)") +
+  ylab("LD (r2)")
+
+
+ggplot(sfs[sfs$mac == FALSE & sfs$hwe == 1e-6 & sfs$miss == .5,], 
+       aes(x = min_allele_count, y = log10(count), color = maf, group = maf)) +
+  geom_line(size = 1) +
+  theme_bw() +
+  facet_wrap(~pop, scale="free") +
+  khroma::scale_color_batlow(name = "MAF filter") +
+  theme(axis.title = element_text(size = 18),
+        axis.text = element_text(size = 14)) +
+  xlab("Minor Allele Count") +
+  ylab("log10(Number of SNPs)")
---
+++ scripts/whalund.R
@@ -0,0 +1,127 @@
+library(ggplot2); library(snpR)
+alpha <- .35
+mon <- readRDS("data/monarch_nomaf.RDS")
+mon <- mon[pop = c("GUA", "ROT")]
+mon <- filter_snps(mon, non_poly = TRUE)
+
+# mon <- calc_ho(mon)
+# mon <- calc_ho(mon, "pop")
+# mon <- calc_he(mon)
+# mon <- calc_he(mon, "pop")
+mon <- calc_fis(mon)
+mon <- calc_fis(mon, "pop")
+mon <- calc_pairwise_fst(mon, "pop")
+mon <- calc_hwe(mon)
+mon <- calc_hwe(mon, "pop")
+
+rp <- get.snpR.stats(mon, "pop", c("fst"))
+rs <- get.snpR.stats(mon, stats = c("ho", "he", "fis", "hwe"))
+
+pd <- merge(rp$pairwise, rs$single, by = c("group", "position"))
+pd$fst[pd$fst < 0] <- 0
+ggplot(pd, aes(x = fst, y = fis)) + geom_point(alpha = 0.5) +
+  geom_smooth() +
+  theme_bw() +
+  ylab(bquote(F[IS])) +
+  xlab(bquote(F[ST])) +
+  theme(axis.text = element_text(size = 16),
+        axis.title = element_text(size = 18))
+
+ggplot(pd, aes(x = fst, y = -log10(pHWE))) + geom_point(alpha = alpha) +
+  geom_smooth() +
+  geom_hline(yintercept = -log10(1e-6), mapping = aes(xmin = 0, xmax = 1), color = "red") +
+  theme_bw() +
+  ylab(bquote(-log[10](p[HWE]))) +
+  xlab(bquote(F[ST])) +
+  theme(axis.text = element_text(size = 16),
+        axis.title = element_text(size = 18))
+
+rsp <- get.snpR.stats(mon, "pop", c("fis", "hwe"))$single
+rsp <- merge(rsp[which(rsp$subfacet == "GUA"),], rsp[which(rsp$subfacet == "ROT"),], by = c("group", "position"), 
+             suffixes = c("_GUA", "_ROT"))
+rsp <- as.data.table(rsp)
+rsp[,mfis := rowMeans(cbind(fis_GUA, fis_ROT), na.rm = TRUE)]
+rsp[,mHWE := rowMeans(cbind(pHWE_ROT, pHWE_GUA), na.rm = TRUE)]
+
+pd2 <- merge(pd, rsp, by = c("group", "position"))
+pd2 <- as.data.table(pd2)
+pd2[,bad_reject := ((pHWE_ROT >= 1e-6 & pHWE_ROT >= 1e-6) & pHWE <= 1e-6)]
+
+ggplot(pd2, aes(x = mfis, y = fis, color = fst)) +
+  geom_point(alpha = alpha) +
+  geom_abline(slope = 1, intercept = 0, color = "red") +
+  khroma::scale_color_batlow() +
+  theme_bw()  +
+  theme(axis.text = element_text(size = 16),
+        axis.title = element_text(size = 18)) +
+  xlab(bquote("Mean" ~ F[IS])) +
+  ylab(bquote("Combined" ~ F[IS]))
+
+ggplot(pd2, aes(x = fst, y = fis - mfis)) +
+  geom_point(alpha = alpha) +
+  khroma::scale_color_batlow() +
+  theme_bw() +
+  geom_smooth() +
+  xlab(bquote(F[ST])) +
+  ylab(bquote("Combined" ~ - "Mean" ~ F[IS])) +
+  theme(axis.text = element_text(size = 16),
+        axis.title = element_text(size = 18))
+
+ggplot(pd2, aes(x = fst, y = pHWE - mHWE)) +
+  geom_point(alpha = alpha, aes(color = bad_reject)) +
+  scale_color_manual(values = c("black", "red")) +
+  theme_bw() +
+  geom_smooth() +
+  xlab(bquote(F[ST])) +
+  ylab(bquote("Combined" ~ - "Mean" ~ p[HWE])) +
+  guides(color = guide_legend(title = "Falsely Rejected")) +
+  theme(axis.text = element_text(size = 16),
+        axis.title = element_text(size = 18))
+
+
+pd3_1 <- melt(pd2[,c("group", "position", "fis", "fis_ROT")], id.vars = c("group", "position"))
+pd3_1$level <- ifelse(grepl("ROT", pd3_1$variable), "Split", "Combined")
+colnames(pd3_1) <- c("group", "position", "variable", "fis", "level")
+pd3_1$variable <- NULL
+
+pd3_2 <- melt(pd2[,c("group", "position", "pHWE", "pHWE_ROT")], id.vars = c("group", "position"))
+pd3_2$level <- ifelse(grepl("ROT", pd3_2$variable), "Split", "Combined")
+colnames(pd3_2) <- c("group", "position", "variable", "pHWE", "level")
+pd3_2$variable <- NULL
+
+pd3_3 <- pd2[,c("group", "position", "fst")]
+
+pd3 <- merge(pd3_1, pd3_2, by = c("group", "position", "level"))
+pd3 <- merge(pd3, pd3_3, by = c("group", "position"))
+
+p <- ggplot(pd3, aes(x = fst, y = fis)) +
+  geom_point(aes(color = -log10(pHWE)), alpha = alpha) +
+  geom_smooth(color = "red", method = "lm") +
+  khroma::scale_color_batlow(range = c(.2, 1)) +
+  theme_bw() +
+  facet_wrap(~level) +
+  ylab(bquote(F[IS])) +
+  xlab(bquote(F[ST])) +
+  theme(axis.text = element_text(size = 16),
+        axis.title = element_text(size = 18),
+        strip.background = element_blank(),
+        strip.text = element_text(size = 20))
+
+ggsave("data/processed/whalund_plot.pdf", p, "pdf", width = 15, height = 11)
+
+
+
+ggplot(mapping = aes(x = fst, y = fis)) +
+  geom_point(data = pd3[which(pd3$pHWE > 0.05),], color = "gray", alpha = alpha/3) +
+  geom_point(data = pd3[which(pd3$pHWE <= 0.05),], aes(color = -log10(pHWE)), alpha = alpha) +
+  geom_smooth(data = pd3, color = "red", method = "lm") +
+  khroma::scale_color_batlow(range = c(.2, 1)) +
+  theme_bw() +
+  facet_wrap(~level) +
+  ylab(bquote(F[IS])) +
+  xlab(bquote(F[ST])) +
+  theme(axis.text = element_text(size = 16),
+        axis.title = element_text(size = 18),
+        strip.background = element_blank(),
+        strip.text = element_text(size = 20))
+
